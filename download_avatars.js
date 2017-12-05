79027474b9e5ba2688672a823b6434d091117cd5
Frank Lam <frank.is.missing@gmail.com>
2017-12-05 07:06:31 +0000

    Added argument checking handling

--- download_avatars.js
+++ download_avatars.js
@@ -1,13 +1,16 @@
 var request = require('request');
 var owner = process.argv[2];
 var nameOfRepo = process.argv[3];
+var extraArgument = process.argv[4];
 var engine = require('./download_avatars_helperFunctions');
 
-if (owner && nameOfRepo) {
-    console.log('Welcome to the GitHub Avatar Downloader');
+if (engine.errorCheck(owner, nameOfRepo, extraArgument) ) {
     engine.gitHubOptions.url += (owner + "/" + nameOfRepo + "/contributors");
     request(engine.gitHubOptions, engine.downloadEngine);
-  } else {
-    console.log("ERROR. Owner and Repo name not provided");
-    console.log("Usage: node download_avatars.js <owner> <repo>");
-    }
+}
+else {
+  console.log("ERROR.");
+    // console.log("Owner and/or Repo name was not provided OR");
+    // console.log("Too many arguments provided");
+    // console.log("Usage: node download_avatars.js <owner> <repo>");
+}
--- download_avatars_helperFunctions.js
+++ download_avatars_helperFunctions.js
@@ -1,7 +1,7 @@
 var request = require('request');
 var fs = require('fs');
 var dotenv = require('dotenv').config();
-
+var path = "/avatars";
 var gitHubOptions = {
     url: "https://api.github.com/repos/",
     json: true,
@@ -12,14 +12,17 @@ var gitHubOptions = {
 }
 
 function downloadEngine (err, response, body){
+  console.log('Welcome to the GitHub Avatar Downloader');
   if (err) {
   console.log("Errors:", err);
   }
+
   if (body){
     body.forEach(function (user){
     downloadImageByURL(user.avatar_url, "avatars/" + user.login + ".jpg");
     });
   }
+
   console.log("Downloads completed into /avatars");
 }
 
@@ -31,5 +34,26 @@ function downloadImageByURL(url, filePath){
   .pipe(fs.createWriteStream(filePath));
 }
 
+function errorCheck(owner, repo, extraArgument){
+  // Checks if there is a 5th argument
+  if (extraArgument !== undefined){
+    console.log("Too many arguments");
+    console.log("Usage: node download_avatars.js <owner> <repo>");
+    return false;
+  }
+  // Checks that owner and repo argument provided
+  if (owner === undefined || repo === undefined) {
+    console.log("Owner and/or Repo name was not provided");
+    console.log("Usage: node download_avatars.js <owner> <repo>");
+    return false;
+  }
+
+  else {
+    return true;
+  }
+
+}
+
 module.exports.gitHubOptions = gitHubOptions;
 module.exports.downloadEngine = downloadEngine;
+module.exports.errorCheck = errorCheck;
