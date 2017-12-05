9811c835c8b0154d791d19b166e2b0f9dcab05b6
Frank Lam <frank.is.missing@gmail.com>
2017-12-05 02:03:47 +0000

    Added minor functionality

--- download_avatars.js
+++ download_avatars.js
@@ -1,12 +1,23 @@
 var request = require('request');
+var GITHUB_TOKEN = require("./secrets.js");
 
 console.log('Welcome to the GitHub Avatar Downloader');
 
-function getRepoContributors(repoOwner, repoName, cb) {
+function getRepoContributors(repoOwner, repoName, callback) {
+  var options = {
+    url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
+    headers: {
+      'User-Agent': 'request'
+      'Authorization':
+    }
+  };
 
+  request(options, function(err, res, body) {
+    cb(err, body);
+  });
 }
 
-getRepoContributors("nodejs", "https://github.com/nodejs/node", function(err, result) {
+getRepoContributors("jquery", "jquery", function(err, result) {
   console.log("Errors:", err);
   console.log("Result:", result);
 });
