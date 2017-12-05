dee9f2d6f20a862b9418e88360feb189dcd5bf01
Frank Lam <frank.is.missing@gmail.com>
2017-12-05 02:34:43 +0000

    Added ability to pull avatar urls

--- download_avatars.js
+++ download_avatars.js
@@ -6,18 +6,24 @@ console.log('Welcome to the GitHub Avatar Downloader');
 function getRepoContributors(repoOwner, repoName, callback) {
   var options = {
     url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
+    json: true,
     headers: {
-      'User-Agent': 'request'
-      'Authorization':
+      'User-Agent': 'request',
+      'Authorization': 'token ' + GITHUB_TOKEN.token
     }
   };
 
   request(options, function(err, res, body) {
-    cb(err, body);
+    callback(err, body);
   });
 }
 
 getRepoContributors("jquery", "jquery", function(err, result) {
   console.log("Errors:", err);
-  console.log("Result:", result);
+  // console.log("Result:", result);
+  if (result){
+    result.forEach(function (user){
+      console.log(user.avatar_url);
+    });
+  }
 });
